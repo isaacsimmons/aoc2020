b8d9c405006cf8fa3ff48fb1216cff7943b6d3b7
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-03 07:58:55 -0500

    3pt2

--- src/day3/main.ts
+++ src/day3/main.ts
@@ -29,20 +29,35 @@ interface Coords {
     y: number;
 }
 
-const step: Coords = { x: 3, y: 1 };
-const position: Coords = { x: 0, y: 0 };
+const countTrees = (step: Coords) => {
+    const position: Coords = { x: 0, y: 0 };
+    let trees = 0;
+    while (position.y < map.height) {
+        if (isTree(map, position)) {
+            trees++;
+        }
+        position.x += step.x;
+        position.y += step.y;
+    }
+    return trees;
+};
 
 const isTree = (m: Map, pos: Coords) => {
     return map.trees[pos.y][pos.x % map.width];
 };
 
-let trees = 0;
-while (position.y < map.height) {
-    if (isTree(map, position)) {
-        console.log('tree at', position);
-        trees++;
-    }
-    position.x += step.x;
-    position.y += step.y;
-}
-console.log('trees: ', trees);
+const steps: Coords[] = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+];
+
+let product = 1;
+steps.forEach(step => {
+    const trees = countTrees(step);
+    console.log(step, trees);
+    product *= trees;
+});
+console.log(product);

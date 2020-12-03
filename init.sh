7aa32f15592b8f36445f88420dc21aa18658de8b
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-03 08:20:34 -0500

    Better init script

--- init.sh
+++ init.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+source .env
+
 day="${1:-}"
 
 [ -z "$day" ] && {
@@ -13,4 +15,8 @@ mkdir "src/day$day"
 cp templates/main.ts "src/day$day"
 
 mkdir "inputs/day$day"
-touch "inputs/day$day/input.txt"
+
+curl "https://adventofcode.com/2020/day/$day/input" \
+  -H "cookie: session=$SESSION_COOKIE" \
+  -o "inputs/day$day/input.txt"
+touch "inputs/day$day/example1.txt"

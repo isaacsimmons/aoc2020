9de14d7b780661819b6df0b84e8dab9698d8687e
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-05 14:46:53 -0500

    Some more helper fns

--- src/day5/main.ts
+++ src/day5/main.ts
@@ -18,6 +18,25 @@ const idToSeat = (id: number): Seat => ({
     column: id % 8,
 });
 
+const leftPad = (s: string, length: number, padding: string) => {
+    if (s.length >= length) {
+        return s;
+    }
+    const paddingLength = length - s.length;
+    let paddingString = padding.repeat(Math.ceil(paddingLength / padding.length));
+
+    if (paddingString.length > paddingLength) {
+        paddingString = paddingString.substr(0, paddingLength);
+    }
+    return paddingString + s;
+};
+
+const seatToBoardingPass = (seat: Seat) => {
+    const row = seat.row.toString(2).replaceAll('1', 'B').replaceAll('0', 'F');
+    const column = seat.column.toString(2).replaceAll('1', 'R').replaceAll('0', 'L');
+    return leftPad(row, 7, 'F') + leftPad(column, 3, 'L');
+};
+
 const seatIDs = inputLines.map(boardingPassToSeatID);
 const min = Math.min(...seatIDs);
 const max = Math.max(...seatIDs);

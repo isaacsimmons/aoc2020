64fbde62917f122566c3b7e546b9ca4231c8e540
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-07 08:31:20 -0500

    7pt1

---
+++ src/day7/main.ts
@@ -0,0 +1,88 @@
+import { truthy } from '../utils/array';
+import { readInputLines } from '../utils/file';
+
+const inputLines = readInputLines().filter(truthy);
+
+interface Rule {
+    color: string;
+    contents: Contents[];
+}
+
+interface Contents {
+    color: string;
+    quantity: number;
+}
+
+const contents = new Map<string, Map<string, number>>();
+const containedIn = new Map<string, Map<string, number>>();
+
+const saveEntry = (map: Map<string, Map<string, number>>, key1: string, key2: string, value: number) => {
+    let inner: Map<string, number>|undefined = map.get(key1);
+    if (!inner) {
+        inner = new Map<string, number>();
+        map.set(key1, inner);
+    }
+    if (inner.has(key2)) {
+        throw new Error(`Duplicate key: ${key1} ${key2}`);
+    }
+    inner.set(key2, value);
+};
+
+const parseContent = (s: string): Contents => {
+    const firstSpace = s.indexOf(' ');
+    const lastSpace = s.lastIndexOf(' ');
+    const quantity = s.substr(0, firstSpace);
+    const color = s.substr(firstSpace + 1, lastSpace - firstSpace - 1);
+    const bags = s.substr(lastSpace + 1);
+    if (bags !== 'bag' && bags !== 'bags') {
+        throw new Error('must end with bag');
+    }
+    return {quantity: Number(quantity), color};
+};
+
+const parseLine = (s: string): Rule => {
+    const [color, right] = s.split(' bags contain ');
+    if (right === 'no other bags.') {
+        return { color, contents: []};
+    }
+    if (right[right.length - 1] !== '.') {
+        throw new Error('no period');
+    }
+    const pieces = right.substr(0, right.length - 1).split(', ');
+    return { color, contents: pieces.map(parseContent)};
+};
+
+inputLines.forEach(line => {
+    const rule = parseLine(line);
+    rule.contents.forEach(({color, quantity}) => {
+        saveEntry(contents, rule.color, color, quantity);
+        saveEntry(containedIn, color, rule.color, quantity);
+    });
+});
+
+console.log(contents);
+
+const result = new Set<string>();
+const active = new Set(['shiny gold']);
+
+const check = () => {
+    const color = active.values().next().value;
+    active.delete(color);
+    if (!containedIn.has(color)) {
+        return;
+    }
+    const newColors = [...containedIn.get(color)!.keys()];
+    newColors.forEach(newColor => {
+        if (!result.has(newColor)) {
+            result.add(newColor);
+            active.add(newColor);
+        }
+    });
+};
+
+while (active.size > 0) {
+    check();
+}
+
+console.log(result);
+console.log(result.size);

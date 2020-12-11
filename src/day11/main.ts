5edfc3a401c9e9da36961ebd78c19477d466858d
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-11 09:31:24 -0500

    Switch to enum, make parser/renderer a bit more generic

--- src/day11/main.ts
+++ src/day11/main.ts
@@ -3,44 +3,31 @@ import { readInputLines } from '../utils/file';
 
 const inputLines = readInputLines();
 
-type Space = 'floor' | 'empty' | 'occupied';
+const enum Space {
+    FLOOR = '.',
+    EMPTY = 'L',
+    OCCUPIED = '#',
+}
 
 const parseSpace = (s: string): Space => {
     switch (s) {
         case '.':
-            return 'floor';
         case 'L':
-            return 'empty';
         case '#':
-            return 'occupied';
+            return s as Space;
         default:
             throw new Error('bad char ' + s);
     }
 };
 
-const renderSpace = (s: Space): string => {
-    switch (s) {
-        case 'empty':
-            return 'L';
-        case 'occupied':
-            return '#';
-        case 'floor':
-            return '.';
-        default:
-            throw new Error('bad space ' + s);
-    }
-};
-
-const inputValues = inputLines.map(row => [...row].map(parseSpace));
-
 const isOccupied = (x: number, y: number, dx: number, dy: number, map: Grid<Space>): number => {
     let d = 1;
     while (true) {
         const space = map.get({x: x + dx * d, y: y + dy * d});
-        if (space === null || space === 'empty') {
+        if (space === null || space === Space.EMPTY) {
             return 0;
         }
-        if (space === 'occupied') {
+        if (space === Space.OCCUPIED) {
             return 1;
         }
         d++;
@@ -56,11 +43,15 @@ const countAdjacentOccupied = (x: number, y: number, map: Grid<Space>): number =
 const iterate = (prev: Grid<Space>): Grid<Space> => {
     const next = prev.copy();
     for (const {x, y, value} of next.iter()) {
+        if (value === Space.FLOOR) {
+            // Little optimization -- no need to count adjacent seats for floor spaces
+            continue;
+        }
         const neighbors = countAdjacentOccupied(x, y, prev);
-        if (value === 'empty' && neighbors === 0) {
-            next.set({x, y}, 'occupied');
-        } else if (value === 'occupied' && neighbors >= 5) {
-            next.set({x, y}, 'empty');
+        if (value === Space.EMPTY && neighbors === 0) {
+            next.set({x, y}, Space.OCCUPIED);
+        } else if (value === Space.OCCUPIED && neighbors >= 5) {
+            next.set({x, y}, Space.EMPTY);
         }
     }
     return next;
@@ -69,19 +60,25 @@ const iterate = (prev: Grid<Space>): Grid<Space> => {
 const countOccupied = (map: Grid<Space>) => {
     const sums = new Counter<Space>();
     sums.addAll([...map.iter()].map(({value}) => value));
-    return sums.get('occupied') || 0;
+    return sums.get(Space.OCCUPIED) || 0;
 }
 
-const writeGrid = (grid: Grid<Space>) => {
+const writeGrid = <T>(grid: Grid<T>, renderer: (value: T) => string) => {
     const rows = grid.to2d();
-    rows.forEach(row => console.log(row.map(renderSpace).join('')));
+    rows.forEach(row => console.log(row.map(renderer).join('')));
     console.log();
 };
 
-let prev = Grid.from2d(inputValues);
+const readGrid = <T>(lines: string[], parser: (s: string) => T): Grid<T> => {
+    const values = lines.map(row => [...row].map(parser));
+    return Grid.from2d(values);
+};
+
+let prev = readGrid(inputLines, parseSpace);
 let next = iterate(prev);
 while (!prev.equals(next)) {
     prev = next;
     next = iterate(prev);
 }
+writeGrid(next, value => value);
 console.log(countOccupied(next));

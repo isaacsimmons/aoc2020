d57eecb02f054516b15bba3ea4592a54346ac900
Isaac Simmons <isaac.simmons@sharpspring.com>
2020-12-16 11:50:13 -0500

    d16p2

--- src/day16/main.ts
+++ src/day16/main.ts
@@ -1,4 +1,4 @@
-import { sum, truthy } from '../utils/array';
+import { product, sum, truthy } from '../utils/array';
 import { readNewlineSeparatedChunks } from '../utils/file';
 
 const [rangesString, myTicketString, otherTicketStrings] = readNewlineSeparatedChunks();
@@ -56,12 +56,77 @@ const isValid = (value: number, {min1, max1, min2, max2}: Range) =>
 const isValidAny = (value: number, ranges: Range[]): boolean => 
     ranges.some(range => isValid(value, range));
 
-const findInvalid = (ticket: Ticket, ranges: Ranges): number[] => {
+const findInvalidValues = (ticket: Ticket, ranges: Ranges): number[] => {
     return ticket.filter(value => !isValidAny(value, [...ranges.values()]));
+};
+
+const isTicketInvalid = (ticket: Ticket, ranges: Ranges): boolean => {
+    return findInvalidValues(ticket, ranges).length === 0;
 };
 
 // console.log(ranges, ownTicket, otherTickets);
 
-const invalids = otherTickets.flatMap(otherTicket => findInvalid(otherTicket, ranges));
-console.log(invalids);
-console.log(sum(invalids));
+const allTickets = [ownTicket, ...otherTickets];
+const allValidTickets = allTickets.filter(ticket => isTicketInvalid(ticket, ranges));
+
+const possibleLabels: Set<string>[] = [];
+for (let idx = 0; idx < ownTicket.length; idx++) {
+    const possible = new Set<string>();
+    for (const [key, range] of ranges.entries()) {
+        const valuesToCheck = allValidTickets.map(ticket => ticket[idx]);
+        const isPossible = valuesToCheck.every(value => isValid(value, range));
+        // console.log(valuesToCheck, 'is', isPossible, 'for', range, '(', key, ')');
+        if (isPossible) {
+            possible.add(key);
+        }
+    }
+    possibleLabels[idx] = possible;
+}
+
+const invalidValues = otherTickets.flatMap(otherTicket => findInvalidValues(otherTicket, ranges));
+console.log('got', invalidValues.length, 'invalid values for a scanning error rate of', sum(invalidValues));
+
+console.log(possibleLabels);
+
+
+const reduceLabels = (possible: Set<string>[]): string[] => {
+    const solution: Array<string|null> = possible.map(labels => null);
+    let n = 0;
+    while (true) {
+        let stuck = true;
+
+        for (let idx = 0; idx < possible.length; idx++) {
+            if (solution[idx] !== null) {
+                continue;
+            }
+
+            if (possible[idx].size === 1) {
+                stuck = false;
+                const label = possible[idx].values().next().value as string;
+                solution[idx] = label;
+                n++;
+                possible.forEach(p => p.delete(label));
+            }
+        }
+
+        if (stuck) {
+            throw new Error('stuck');
+        }
+
+        if (n === solution.length) {
+            return solution as string[];
+        }
+    }
+};
+
+const labelAssignments = reduceLabels(possibleLabels);
+console.log(labelAssignments);
+
+const solution = product(labelAssignments.map((labelAssignment, idx) => {
+    if (labelAssignment.substring(0, 9) !== 'departure') {
+        return 1; // 1 to basically "skip" the value in the product
+    }
+    return ownTicket[idx];
+}));
+
+console.log(solution);
